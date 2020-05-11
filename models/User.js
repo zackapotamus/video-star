ea99e5ee4aced9e628c7b25e3f88ae1a4d2db4c5
Zack Hayden <signbarn@gmail.com>
2020-05-11 10:08:45 -0400

    back to snake case

--- models/User.js
+++ models/User.js
@@ -1,3 +1,5 @@
+var bcrypt = require("bcryptjs");
+
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     name: DataTypes.STRING,
@@ -14,11 +16,22 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false
     }
+  }, {
+    underscored: true,
+    freezeTableName: true
+  });
+
+  User.prototype.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+
+  User.addHook("beforeCreate", function(user) {
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
   });
 
   User.associate = function(models) {
     User.hasMany(models.Video, {
-      foreignKey: "userId"
+      foreignKey: "user_id"
     });
   };
   return User;
