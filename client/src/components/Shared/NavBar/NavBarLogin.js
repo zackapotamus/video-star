4079a1144c80ae702a4a4d0c7a300f9b78675700
Tomas Perez <tomaspz@gmail.com>
2020-05-11 14:03:14 -0400

    add bootstrap cdn and jquery to index.html

--- client/src/components/Shared/NavBar/NavBarLogin.js
+++ client/src/components/Shared/NavBar/NavBarLogin.js
@@ -2,29 +2,32 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faUser, faComment, faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import "./NavBar.css"
+import "./NavBar.css";
 
 function NavBarLogin() {
 
   const location = useLocation();
 
   return (
-    <nav className="navbar navbar-expand-lg border-bottom border-light ml-auto">
-    
-      <img src="./assets/img/videostar-logo.png" alt="logo" style={{ marginLeft: 40, width: 200 }} />
+    <nav className="navbar navbar-expand-lg navbar-light bg-light ml-auto">
       
+      <a className="navbar-brand" href="/">
+        <img src="./assets/img/videostar-logo.png" alt="logo" style={{ marginLeft: 20, marginTop: 20, width: 200 }} />
+      </a>
       
-      <div className="navbar-collapse" id="navbarSupportedContent">
-          <ul className="nav navbar-nav" style={{display: 'block', alignContent: 'right'}}>
+      <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="nav navbar-nav ml-auto">
+
             <li className="nav-item">
-            <Link to="/signup" className={location.pathname === "/signup" ? "nav-link active" : "nav-link"}><span className="clearfix d-none d-sm-inline-block" />Sign Up</Link>
+              <Link to="/signup" className={location.pathname === "/signup" ? "nav-link active" : "nav-link"}><span className="clearfix d-none d-sm-inline-block" />Sign Up</Link>
             </li>
+
             <li className="nav-item">
-            <Link to="/login" className={location.pathname === "/login" ? "nav-link active" : "nav-link"}><span className="clearfix d-none d-sm-inline-block" />Login</Link>
+              <Link to="/login" className={location.pathname === "/login" ? "nav-link active" : "nav-link"}><span className="clearfix d-none d-sm-inline-block" />Login</Link>
             </li>
+
           </ul>
       </div> 
-     
 
     </nav>
   );
