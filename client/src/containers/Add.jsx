2f44e390823050aefce779b70e1e480b644fe21f
Brandon Kim <bkim377@gatech.edu>
2020-05-13 11:34:30 -0400

    added Hero images to several pages

--- client/src/containers/Add.jsx
+++ client/src/containers/Add.jsx
@@ -1,39 +1,46 @@
-import React, { Component } from 'react';
-import NavBar2 from '../components/Shared/NavBar/NavBar2';
-import GreyBlockTop from '../components/Shared/GreyBlockTop/GreyBlockTop';
-import GreyBlock from '../components/Shared/GreyBlock/GreyBlock';
+import React, { Component } from "react";
+import NavBar2 from "../components/Shared/NavBar/NavBar2";
+import Hero from '../components/Shared/Hero/Hero';
+import GreyBlockTop from "../components/Shared/GreyBlockTop/GreyBlockTop";
+import GreyBlock from "../components/Shared/GreyBlock/GreyBlock";
+
+import AddMovie from "../img/add-movie.jpg";
 
 class Add extends Component {
-    render() {
-        return (
-            <>
-            <NavBar2 />
-            <GreyBlockTop page='Add'/>
+  render() {
+    return (
+      <>
+        <NavBar2 />
+        <Hero imageUrl={AddMovie}/>
+        <GreyBlockTop page="Add" />
 
-            <div className="container" style={{marginBottom: 100}}>
-            <div class="row">
-              <div class="col-sm-1"></div>
-                <div class="col-sm-10">
-                <div className="card">
-                    <div className = "card-body">
-                        <h2 className="card-title">Add a Video to your Library</h2>
-                    </div>
+        <div className="container" style={{ marginBottom: 100 }}>
+          <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-10">
+              <div className="card">
+                <div className="card-body">
+                  <h2 className="card-title">Add a Video to your Library</h2>
+                </div>
                 <form>
-                            <div className="form-group px-5">
-                              <label for="exampleFormControlInput1">Movie Title</label>
-                              <input type="title" className="form-control" id="exampleFormControlInput1" placeholder="Search for a Movie">
-                            </input>
-                            
+                  <div className="form-group px-5">
+                    <label for="exampleFormControlInput1">Movie Title</label>
+                    <input
+                      type="title"
+                      className="form-control"
+                      id="exampleFormControlInput1"
+                      placeholder="Search for a Movie"
+                    ></input>
                   </div>
                 </form>
+              </div>
             </div>
-            </div>
-            </div>
-            </div>
-            <GreyBlock />
-            </>
-        );
-    }
+          </div>
+        </div>
+        <GreyBlock />
+      </>
+    );
+  }
 }
 
 export default Add;
