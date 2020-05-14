03cab9d06ee9e5e11b8cb350b096fca135925a4b
Tomas Perez <tomaspz@gmail.com>
2020-05-14 10:51:46 -0400

    comment out font awesome in index.html

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import logo from './assets/img/videostar-logo-master.svg';
-import "./App.css";
+// import "./App.css";
 import axios from "axios";
 import jwt from "jsonwebtoken";
 import Signup from "./containers/Signup";
