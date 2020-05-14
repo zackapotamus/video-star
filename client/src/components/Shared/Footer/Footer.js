872f9f8027d1a457b330212c031952c54ee90645
Brandon Kim <bkim377@gatech.edu>
2020-05-14 13:48:40 -0400

    added MovieDB logo to footer

--- client/src/components/Shared/Footer/Footer.js
+++ client/src/components/Shared/Footer/Footer.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./Footer.css";
+import MovieDBLogo from "../../../img/moviedb-logo.png";
 
 function Footer() {
-  
-    return (
-        <footer className="page-footer font-small fixed-bottom pt-4">
-        <div className="footer-copyright text-center py-3">
-            © Copyright VideoStar 2020
-        </div>
+  return (
+    <footer className="page-footer font-small fixed-bottom pt-4">
+      <div className="footer-copyright text-center py-3">
+      <img className="mr-3" src={MovieDBLogo} alt="moviedb-logo" style={{ width: 204 }}/>
+        © Copyright VideoStar 2020
+      </div>
     </footer>
-    );
-  }
-  
-  export default Footer;
+  );
+}
+
+export default Footer;
