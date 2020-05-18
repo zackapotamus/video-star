0737ba89b2d743fb375d16d0c3022b47d64665a1
Tomas Perez <tomaspz@gmail.com>
2020-05-18 10:21:04 -0400

    added favicon.ico and the video_genre_seed.sql

---
+++ scripts/video_genre_seed.sql
@@ -0,0 +1,22 @@
+USE videostar_db;
+
+INSERT INTO Video_Genre (video_id, genre_id) 
+VALUES
+(1, 28),
+(1, 878),
+(2, 12),
+(2, 14),
+(3, 28),
+(3, 878),
+(3, 9648),
+(4, 18),
+(4, 28),
+(4, 12),
+(5, 878),
+(5, 28),
+(5, 12),
+(6, 878),
+(6, 28),
+(6, 12);
+
+SELECT * FROM Video_Genre;
