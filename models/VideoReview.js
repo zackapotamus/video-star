ae5d59486d0e834a320a175c5fc87cdee794b55e
Zack Hayden <signbarn@gmail.com>
2020-06-15 19:36:03 -0400

    formatting

--- models/VideoReview.js
+++ models/VideoReview.js
@@ -1,23 +1,33 @@
 module.exports = (sequelize, DataTypes) => {
-  const VideoReview = sequelize.define("Video_Review", {
-    video_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      foreignKey: true
+  const VideoReview = sequelize.define(
+    "Video_Review",
+    {
+      video_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        foreignKey: true,
+      },
+      review_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        foreignKey: true,
+      },
     },
-    review_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      foreignKey: true
+    {
+      timestamps: false,
+      underscored: true,
+      freezeTableName: true,
     }
-  }, {
-    timestamps: false,
-    underscored: true,
-    freezeTableName: true
-  });
+  );
   VideoReview.associate = (models) => {
-    VideoReview.belongsTo(models.Video, {foreignKey: "video_id", targetKey: "id"});
-    VideoReview.belongsTo(models.Review, {foreignKey: "review_id", targetKey: "id"});
+    VideoReview.belongsTo(models.Video, {
+      foreignKey: "video_id",
+      targetKey: "id",
+    });
+    VideoReview.belongsTo(models.Review, {
+      foreignKey: "review_id",
+      targetKey: "id",
+    });
   };
   return VideoReview;
 };
