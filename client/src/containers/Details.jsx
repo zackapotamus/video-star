51cc1b397dfbd1f1f243983296dbdc79f0b20923
Zack Hayden <signbarn@gmail.com>
2022-09-23 00:20:07 -0400

    slight cleanup

--- client/src/containers/Details.jsx
+++ client/src/containers/Details.jsx
@@ -1,47 +1,34 @@
 import React, { Component } from "react";
-// import NavBar2 from "../components/Shared/NavBar/NavBar2";
-import NavBarNew from "../components/Shared/NavBar/NavBarNew";
-import Hero from "../components/Shared/Hero/Hero";
-import GreyBlockTop from "../components/Shared/GreyBlockTop/GreyBlockTop";
 import GreyBlock from "../components/Shared/GreyBlock/GreyBlock";
-// import MovieCard from "../components/Shared/Card/MovieCard";
 import API from "../utils/API";
-import EmptyMovieSeats from "../img/cinema-empty-seats.jpg";
 import DetailsCard from "../components/Shared/Card/DetailsCard";
 
 class Details extends Component {
   constructor() {
     super();
     this.state = {
-      video: { genres: [], cast: [], runtime: 0, directors: [], release_date: new Date()},
+      video: {
+        id: this.props.match.params.id || 0,
+        genres: [],
+        cast: [],
+        runtime: 0,
+        directors: [],
+        release_date: new Date(),
+      },
     };
     this.componentDidMount = this.componentDidMount.bind(this);
+    this.deleteVideo = this.deleteVideo.bind(this);
   }
 
+  async deleteVideo()
+
   async componentDidMount() {
-    let result = await API.getVideo(this.props.match.params.id/*, token*/);
+    let result = await API.getVideo(this.props.match.params.id /*, token*/);
     this.setState({ video: result.data });
   }
 
   render() {
-    return (
-      <>
-        {/* <NavBar2 /> */}
-        {/* <NavBarNew /> */}
-        {/* <Hero imageUrl={EmptyMovieSeats} /> */}
-        {/* <GreyBlockTop page="Details" /> */}
-        {/* <div className="container-fluid bg-light py-3"> */}
-          {/* <div className="row"> */}
-            {/* <div className="col-md-6 mx-auto"> */}
-              {/* <MovieCard video={this.state.video}/> */}
-              <DetailsCard video={this.state.video} />
-            {/* </div> */}
-          {/* </div> */}
-        {/* </div> */}
-
-        <GreyBlock />
-      </>
-    );
+    return <DetailsCard video={this.state.video} />;
   }
 }
 
